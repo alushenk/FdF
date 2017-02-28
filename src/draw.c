5f24e9b1be342cb7eed8a4719b3c87000933c715
Anton Lushenko <alushenk@e1r1p17.unit.ua>
2017-02-28 18:09:26 +0200

    gradient works more correctly, but still wrong

--- src/draw.c
+++ src/draw.c
@@ -4,14 +4,14 @@
 
 #include "FdF.h"
 
-static void    write_pixel(int x, int y, int *color, t_struct *mlx)
+static void    write_pixel(int x, int y, double *color, t_struct *mlx)
 {
     int a;
 
     a = ((int)y * mlx->line_size + ((int)x * (mlx->bits_per_pixel / 8)));
-    mlx->image_data[a] = color[0];
+    mlx->image_data[a] = color[2];
     mlx->image_data[a + 1] = color[1];
-    mlx->image_data[a + 2] = color[2];
+    mlx->image_data[a + 2] = color[0];
 }
 
 static int     in_range(int x, int y)
@@ -21,11 +21,11 @@ static int     in_range(int x, int y)
     return (1);
 }
 
-void     get_color(int *color, int start, int end, int dist)
+void     get_color(double *color, int start, int end, double dist)
 {
-    color[0] = ((char)start>>(2 * 8)) - ((char)end>>(2 * 8));
-    color[1] = ((char)start>>(1 * 8)) - ((char)end>>(1 * 8));
-    color[2] = (char)start - (char)end;
+    color[0] = (((unsigned char)(end>>(2 * 8)) - (unsigned char)(start>>(2 * 8))));
+    color[1] = (((unsigned char)(end>>(1 * 8)) - (unsigned char)(start>>(1 * 8))));
+    color[2] = (unsigned char)start - (unsigned char)end;
     if (dist)
     {
         color[0] /= dist;
@@ -34,15 +34,12 @@ void     get_color(int *color, int start, int end, int dist)
     }
 }
 
-static void    draw_line(t_pixel *start, t_pixel *end, t_struct *mlx)
+static void    draw_line(t_pixel *start, t_pixel *end, t_struct *mlx, double *color, double *color_step)
 {
     int     move_x;
     int     move_y;
     int     delta_x;
     int     delta_y;
-
-    int     *step_color;
-    int     *color;
 
     int     error;
     int     error2;
@@ -50,6 +47,15 @@ static void    draw_line(t_pixel *start, t_pixel *end, t_struct *mlx)
     int     y;
     int     x1;
     int     y1;
+
+    //((start->color == 8554658) && (end->color == 15777215))
+    //if (!in_range(start->x, start->y) && !in_range(end->x, end->y))
+    //    return ;
+
+    if (end->color == 0)
+        end->color = 16777215;
+    if (start->color == 0)
+        start->color = 16777215;
 
 
 
@@ -63,15 +69,11 @@ static void    draw_line(t_pixel *start, t_pixel *end, t_struct *mlx)
     delta_y = abs(y1 - y);
     error = delta_x - delta_y;
 
-    step_color = (int*)malloc(sizeof(int) * 3);
-    color = (int*)malloc(sizeof(int) * 3);
-    get_color(step_color, start->color, end->color, (delta_x > delta_y) ? delta_x : delta_y);
-    color[0] = (char)start>>(2 * 8);
-    color[1] = (char)start>>(1 * 8);
-    color[2] = (char)start;
+    color[0] = (unsigned char)(start->color>>(2 * 8));
+    color[1] = (unsigned char)(start->color>>(1 * 8));
+    color[2] = (unsigned char)start->color;
+    get_color(color_step, start->color, end->color, sqrt(pow((end->x - start->x), 2) + pow((end->y - start->y), 2)));
 
-    //if (in_range(end->x, end->y))
-    //    write_pixel(end->x, end->y, end->color, mlx);
     while (x != x1 || y != y1)
     {
         if (in_range(x, y))
@@ -87,19 +89,23 @@ static void    draw_line(t_pixel *start, t_pixel *end, t_struct *mlx)
             error += delta_x;
             y += move_y;
         }
-        color[0] += step_color[0];
-        color[1] += step_color[1];
-        color[2] += step_color[2];
+        color[0] += color_step[0];
+        color[1] += color_step[1];
+        color[2] += color_step[2];
     }
-    free(step_color);
-    free(color);
+    if (in_range(end->x, end->y))
+        write_pixel(end->x, end->y, color, mlx);
 }
 
 static void    draw_map(t_struct *mlx)
 {
     t_pixel *y;
     t_pixel *x;
+    double     *color_step;
+    double     *color;
 
+    color = (double*)malloc(sizeof(double) * 3);
+    color_step = (double*)malloc(sizeof(double) * 3);
     y = mlx->pixel;
     while (y->down)
     {
@@ -107,15 +113,17 @@ static void    draw_map(t_struct *mlx)
         while (x->right)
         {
             if (x->right && x->right->x)
-                draw_line(x, x->right, mlx);
+                draw_line(x, x->right, mlx, color, color_step);
             if (x->down && x->down->y)
-                draw_line(x, x->down, mlx);
-            if (x->right && x->right->down && x->right->down->x && x->right->down-y)
-                draw_line(x, x->right->down, mlx);
+                draw_line(x, x->down, mlx, color, color_step);
+            //if (x->right && x->right->down && x->right->down->x && x->right->down-y)
+            //    draw_line(x, x->right->down, mlx, color, color_step);
             x = x->right;
         }
         y = y->down;
     }
+    free(color);
+    free(color_step);
 }
 
 void    draw(t_struct *mlx)
